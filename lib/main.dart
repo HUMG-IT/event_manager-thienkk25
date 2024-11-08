be010606c85396c3e2110a9e5df9a762a9796a09
thienkk25 <thiennguyendz25@gmail.com>
2024-11-08 13:19:48 +0700

    Lưu ngôn ngữ khi thay đổi khi mở app lại

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:localstore/localstore.dart';
 import 'event/event_view.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -16,11 +17,30 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  Locale _locale = const Locale('vi');
+  final db = Localstore.instance;
+  Locale _locale = const Locale('vi'); // Mặc định Vietnamese
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocale();
+  }
+
+  Future<void> _loadLocale() async {
+    final localeData = await db.collection('settings').doc('locale').get();
+    if (localeData != null && localeData['language_code'] != null) {
+      setState(() {
+        _locale = Locale(localeData['language_code']);
+      });
+    }
+  }
 
   void _changeLanguage(Locale locale) {
     setState(() {
       _locale = locale;
+    });
+    db.collection('settings').doc('locale').set({
+      'language_code': locale.languageCode,
     });
   }
 
